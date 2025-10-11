ea643cf6edeede7e2cab34b157b5b050ce0eecb9
Olexandr <Mega.alex.alex2000@gmail.com>
2025-10-12 00:43:49 +0300

    add test and fix workflow

---
+++ src/test/java/demoapp/azuredemokv/service/TestServiceTest.java
@@ -0,0 +1,23 @@
+package demoapp.azuredemokv.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith({MockitoExtension.class})
+class TestServiceTest {
+    @InjectMocks
+    private TestService testService;
+
+    @Test
+    void testMultiply() {
+        long a = 2;
+        long b = 2;
+        long sumMultiply = 4;
+        long result = testService.multiply(a, b);
+        assertEquals(sumMultiply, result);
+    }
+
+}
